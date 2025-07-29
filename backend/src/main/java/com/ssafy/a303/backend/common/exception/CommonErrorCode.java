f9aaa51bb5127f30f0a42e6a8e1363477c453305
gmjang6349 <gmjang6349@gmail.com>
2025-07-29 15:05:39 +0900

    feat: CommonErrorCode 수정 (#2)
    - 찾을 수 없는 리소스에 대한 코드가 없어서 추가

--- backend/src/main/java/com/ssafy/a303/backend/common/exception/CommonErrorCode.java
+++ backend/src/main/java/com/ssafy/a303/backend/common/exception/CommonErrorCode.java
@@ -7,7 +7,8 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_INPUT(400, "입력값이 올바르지 않습니다."),
     AUTHORIZATION_REQUIRED(401, "인증이 필요합니다."),
     FORBIDDEN_REQUEST(403, "권한이 불충분합니다."),
-    RESOURCE_ALREADY_EXIST(409, "이미 존재하는 리소스입니다.");
+    RESOURCE_ALREADY_EXIST(409, "이미 존재하는 리소스입니다."),
+    RESOURCE_NOT_FOUND(404, "이미 존재하는 리소스입니다.");
 
     private final int statusCode;
     private final String message;
