56c537c7e47ad39480424e5231712aab7a8211c9
Joonho <153359718+munjh61@users.noreply.github.com>
2025-07-29 14:28:02 +0900

    input border 삭제

--- frontend/src/components/variants/InputVariants.ts
+++ frontend/src/components/variants/InputVariants.ts
@@ -1,21 +1,18 @@
 import { cva } from "class-variance-authority";
 
-export const InputVariants = cva(
-  "w-full py-2 m-1 text-gray-500 bg-white border border-gray-200 focus:outline-none focus:border-2 focus:border-purple-400 transition-all",
-  {
-    variants: {
-      scale: {
-        sm: "text-sm rounded-sm px-6",
-        md: "text-base rounded-md px-7",
-        lg: "text-lg rounded-lg px-8",
-        xl: "text-xl rounded-xl px-10",
-      },
-      text: {
-        black: "text-black",
-      },
+export const InputVariants = cva("w-full py-2 m-1 text-gray-500 bg-white", {
+  variants: {
+    scale: {
+      sm: "text-sm rounded-sm px-6",
+      md: "text-base rounded-md px-7",
+      lg: "text-lg rounded-lg px-8",
+      xl: "text-xl rounded-xl px-10",
     },
-    defaultVariants: {
-      scale: "md",
+    text: {
+      black: "text-black",
     },
-  }
-);
+  },
+  defaultVariants: {
+    scale: "md",
+  },
+});
