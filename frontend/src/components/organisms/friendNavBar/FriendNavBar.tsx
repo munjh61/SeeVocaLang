fac87610afcdebca9714b3a30d5d635cf5a0cc52
권주현 <joohyun0913@gmail.com>
2025-08-01 17:05:10 +0900

    임시저장

--- frontend/src/components/organisms/friendNavBar/FriendNavBar.tsx
+++ frontend/src/components/organisms/friendNavBar/FriendNavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import SearchIcon from "../../../asset/search.svg?react";
 import BellIcon from "../../../asset/bell.svg?react";
 
@@ -6,11 +6,14 @@ import { NavTab } from "../../molecules/friend/NavTab";
 
 type TabKey = "search" | "friend" | "request";
 
-export const FriendNavBar = () => {
-  const [selectedTab, setSelectedTab] = useState<TabKey>("friend");
+type FriendNavBarProps = {
+  selectedTab: TabKey;
+  setSelectedTab: (tab: TabKey) => void;
+};
 
+export const FriendNavBar = ({ selectedTab, setSelectedTab }: FriendNavBarProps) => {
   return (
-    <div className="flex gap-2 px-4 py-2 bg-white border-b sticky top-0 z-50">
+    <div className="flex gap-2 px-4 py-2 bg-white sticky top-0 z-50 justify-end">
       <NavTab
         label="사람 검색"
         icon={<SearchIcon className="w-4 h-4" />}
@@ -19,7 +22,6 @@ export const FriendNavBar = () => {
       />
       <NavTab
         label="내 친구"
-        // icon={<UserIcon className="w-4 h-4" />}
         selected={selectedTab === "friend"}
         onClick={() => setSelectedTab("friend")}
       />
--- frontend/src/pages/TestPageKwon.tsx
+++ frontend/src/pages/TestPageKwon.tsx
@@ -1,31 +1,30 @@
 import { useState } from "react";
-import { FriendSearchBar } from "../components/molecules/friendSearchBar/FriendSearchBar";
-import { AddFriendButton } from "../components/molecules/friendButtons/AddFriendButton";
-import { DeleteFriendButton } from "../components/molecules/friendButtons/DeleteFriendButton";
-import { RequestFriendButton } from "../components/molecules/friendButtons/RequestFriendButton";
-import { FriendInfoCard } from "../components/molecules/FriendInfoCard/FriendInfoCard";
-import { SearchbarSegment } from "../components/organisms/searchbarSegment/SearchbarSegment";
-import { FriendNavBar } from "../components/organisms/friend/FriendNavBar";
+import { FriendHeader } from "../components/organisms/friendHeader/FriendHeader";
+import { FriendSearchContent } from "../components/templates/friendContents/FriendSearchContent";
+import { MyFriendsContent } from "../components/templates/friendContents/MyFriendsContent";
+import { FriendRequestContent } from "../components/templates/friendContents/FriendRequestContent";
 
-
+type TabKey = "search" | "friend" | "request";
 function TestPageKwon() {
-   const [keyword, setKeyword] = useState("");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.target.value);
-  };
+const [searchValue, setSearchValue] = useState("");
+const [selectedTab, setSelectedTab] = useState<TabKey>("search")
   return(
  
-    <div>
-      <FriendNavBar/>
-      <SearchbarSegment onSearch={v=>console.log(v)}/>
-      <AddFriendButton data="user123" className="w-full"/>
-      <DeleteFriendButton data="user123" className="w-full"/>
-      <RequestFriendButton className="w-full"/>
-      <FriendInfoCard name="권주현" status="none"/>
-      <FriendSearchBar value={keyword} onChange={handleChange} />
+     <div className="p-4">
+      <FriendHeader
+        searchValue={searchValue}
+        onSearchChange={(e) => setSearchValue(e.target.value)}
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+      />
+
+      { <div className="mt-4">
+        {/* {selectedTab === "search" && <FriendSearchContent searchValue={searchValue} />} */}
+        {selectedTab === "friend" && <MyFriendsContent />}
+        {selectedTab === "request" && <FriendRequestContent />}
+      </div> }
     </div>
-    
+     
 
     
 
