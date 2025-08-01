9b319af9f156beefc9c44865806ed91bcb0905cf
Joonho <junho9661@gmail.com>
2025-08-01 14:50:48 +0900

    클릭

--- frontend/src/components/organisms/vocaCard/VocaCard.tsx
+++ frontend/src/components/organisms/vocaCard/VocaCard.tsx
@@ -1,20 +1,40 @@
 import { Text } from "../../atoms/text/Text";
 import { IconButton } from "../../molecules/iconButton/IconButton";
 import { ImageBox } from "../../molecules/imagebox/Imagebox";
+import { Icon } from "../../atoms/icon/Icon";
 import gear from "../../../asset/gear.svg?react";
 import book from "../../../asset/nav_book.svg?react";
+import starF from "../../../asset/star-fill.svg?react";
+import starE from "../../../asset/star_empty.svg?react";
+import { useState } from "react";
 
 type VocaCardProps = {
   thumbnail?: string;
   name: string;
   description: string;
+  favorite: boolean;
 };
 
-export const VocaCard = ({ thumbnail, name, description }: VocaCardProps) => {
+export const VocaCard = ({
+  thumbnail,
+  name,
+  description,
+  favorite,
+}: VocaCardProps) => {
+  const [isFavorite, setIsFavorite] = useState(favorite);
+  const toggleFavorite = () => {
+    setIsFavorite(prev => !prev);
+  };
+
   return (
     <div className="rounded-md shadow-md w-60 p-3 inline-flex flex-col gap-2 bg-white">
-      <div>
-        <ImageBox src={thumbnail} className="w-full h-full"></ImageBox>
+      <div className="relative">
+        <Icon
+          icon={isFavorite ? starF : starE}
+          className="absolute top-0 right-0"
+          onClick={toggleFavorite}
+        />
+        <ImageBox src={thumbnail} className="w-full h-full" />
       </div>
 
       <div className="flex flex-col">
@@ -45,7 +65,7 @@ export const VocaCard = ({ thumbnail, name, description }: VocaCardProps) => {
           </IconButton>
           <IconButton
             IconVariant={{ icon: gear }}
-            ButtonVariant={{ bgColor: "white", border: "purple" }}
+            ButtonVariant={{ bgColor: "white", border: "gray" }}
           >
             수정
           </IconButton>
