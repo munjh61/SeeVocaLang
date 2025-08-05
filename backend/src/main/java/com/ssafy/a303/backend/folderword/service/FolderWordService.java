2a6fcafb194118492c31ec57ca3c28e86f30d7c1
이인우 <dpfdlalfm@naver.com>
2025-08-05 15:48:53 +0900

    feat: FolderWordService 추가
    - 단어와 폴더를 id로 매핑해 저장하는 로직을 service를 추가했습니다.
    
    Resolves: #24

---
+++ backend/src/main/java/com/ssafy/a303/backend/folderword/service/FolderWordService.java
@@ -0,0 +1,26 @@
+package com.ssafy.a303.backend.folderword.service;
+
+import com.ssafy.a303.backend.folder.entity.FolderEntity;
+import com.ssafy.a303.backend.folderword.entity.FolderWordEntity;
+import com.ssafy.a303.backend.folderword.repository.FolderWordRepository;
+import com.ssafy.a303.backend.word.entity.WordEntity;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FolderWordService {
+
+    private final FolderWordRepository folderWordRepository;
+
+    @Transactional
+    public void saveWordInFolder(WordEntity wordEntity, FolderEntity folderEntity) {
+        FolderWordEntity folderWordEntity = FolderWordEntity.builder()
+                .word(wordEntity)
+                .folder(folderEntity)
+                .build();
+
+        folderWordRepository.save(folderWordEntity);
+    }
+}
