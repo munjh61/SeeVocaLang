069a0e36aed80c18a73e7da6c4fe72a4270670d9
이인우 <dpfdlalfm@naver.com>
2025-08-07 13:03:44 +0900

    feat: 오늘의 퀴즈 Service 객체 추가
    - 오늘의 퀴즈를 담당하는 Service 객체를 추가했습니다.
    - 퀴즈 현황 저장, 유저 퀴즈 현황, 오늘의 퀴즈 목록을 가져오는 로직을 가집니다.
    
    Resolves: #30

---
+++ backend/src/main/java/com/ssafy/a303/backend/quiz/service/QuizService.java
@@ -0,0 +1,30 @@
+package com.ssafy.a303.backend.quiz.service;
+
+import com.ssafy.a303.backend.quiz.entity.QuizEntity;
+import com.ssafy.a303.backend.quiz.exception.TodayQuizNotExistRuntimeException;
+import com.ssafy.a303.backend.quiz.repository.QuizRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuizService {
+
+    private final QuizRepository quizRepository;
+
+    public QuizEntity getQuiz(int quizNumber, LocalDateTime currentTime, LocalDateTime endTime) {
+        return quizRepository.findByProblemNumberAndCreatedAtGreaterThanEqualAndCreatedAtLessThan(quizNumber, currentTime, endTime)
+                .orElseThrow(TodayQuizNotExistRuntimeException::new);
+    }
+
+    public int getTodayTotalQuizCount(LocalDateTime currentTime, LocalDateTime endTime) {
+        return quizRepository.countByCreatedAtGreaterThanEqualAndCreatedAtLessThan(currentTime, endTime);
+    }
+
+    public List<QuizEntity> getTodayQuizList(int problemNumber, LocalDateTime startTime, LocalDateTime endTime) {
+        return quizRepository.findAllByProblemNumberGreaterThanEqualAndCreatedAtBetween(problemNumber, startTime, endTime);
+    }
+}
