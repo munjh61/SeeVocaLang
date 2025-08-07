f60b0c8f9b647a89a7f0b6881041dc8fa87e560f
권주현 <joohyun0913@gmail.com>
2025-08-07 09:23:03 +0900

    커밋

--- frontend/src/api/MyPageApi.ts
+++ frontend/src/api/MyPageApi.ts
@@ -1,3 +1,16 @@
 import axios from "axios";
 import { BASE_URL } from "../types/Regex";
 
+const   MYPAGE_URL = `${BASE_URL}/api/v1/users`;
+
+//회원탈퇴
+export const deleteAccount =async():Promise<boolean> =>{
+    try{
+        const response =await axios.delete(`${MYPAGE_URL}`);
+        return response.status ===200;
+    }catch{
+        return false;
+    }
+};
+
+
