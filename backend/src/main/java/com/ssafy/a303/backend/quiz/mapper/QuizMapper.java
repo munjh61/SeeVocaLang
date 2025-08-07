e5759dba0898fc79344d277e61984fdadf44bb93
이인우 <dpfdlalfm@naver.com>
2025-08-07 13:02:03 +0900

    feat: 오늘의 퀴즈 관련 Mapper 객체 추가
    - 오늘의 퀴즈와 관련된 Mapper 객체를 추가했습니다.
    
    Resolves: #30

---
+++ backend/src/main/java/com/ssafy/a303/backend/quiz/mapper/QuizMapper.java
@@ -0,0 +1,18 @@
+package com.ssafy.a303.backend.quiz.mapper;
+
+import com.ssafy.a303.backend.quiz.dto.GetTodayQuizStatusResponseDto;
+import com.ssafy.a303.backend.quiz.dto.GetTodayQuizStatusResultDto;
+import com.ssafy.a303.backend.quiz.dto.UpdateTodayQuizCommandDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDateTime;
+
+@Mapper
+public interface QuizMapper {
+    QuizMapper INSTANCE = Mappers.getMapper(QuizMapper.class);
+
+    GetTodayQuizStatusResponseDto toGetTodayQuizStatusResponseDto(GetTodayQuizStatusResultDto getTodayQuizStatusResultDto);
+
+    UpdateTodayQuizCommandDto toUpdateTodayQuizCommandDto(Long userId, Integer quizNumber, LocalDateTime currentTime);
+}
