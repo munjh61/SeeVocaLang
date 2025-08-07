5483f20c26b7e5df7531f8a54df4f2e1a7ed503a
Joonho <junho9661@gmail.com>
2025-08-07 14:08:02 +0900

    단어장, 단어장 퀴즈, 퀴즈 종료창 구현

--- frontend/src/pages/TestPageMoon.tsx
+++ frontend/src/pages/TestPageMoon.tsx
@@ -5,7 +5,12 @@ function TestPageMoon() {
   return (
     <div className="flex flex-col h-screen">
       <div className="flex grow overflow-y-auto">
-        <QuizDoneTemplate bookname={"연습용"} size={10} result={7} day={10} />
+        <QuizDoneTemplate
+          bookname={"연습용ㅎㅎ"}
+          size={10}
+          result={7}
+          day={10}
+        />
       </div>
       <Navigation loc="book" />
     </div>
