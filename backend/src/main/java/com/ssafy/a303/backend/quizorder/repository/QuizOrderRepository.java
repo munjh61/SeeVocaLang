f890192b230ceb40f2ac9c9e78f711cee532b466
이인우 <dpfdlalfm@naver.com>
2025-08-07 13:02:38 +0900

    feat: 오늘의 퀴즈 순서 Repository 객체 추가
    - 오늘의 퀴즈의 현재 유저 진행상황이 저장된 테이블에 사용되는 Repository 객체를 추가했습니다.
    
    Resolves: #30

---
+++ backend/src/main/java/com/ssafy/a303/backend/quizorder/repository/QuizOrderRepository.java
@@ -0,0 +1,23 @@
+package com.ssafy.a303.backend.quizorder.repository;
+
+import com.ssafy.a303.backend.quiz.entity.QuizEntity;
+import com.ssafy.a303.backend.quizorder.entity.QuizOrderEntity;
+import com.ssafy.a303.backend.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface QuizOrderRepository extends JpaRepository<QuizOrderEntity, Long> {
+
+    Optional<QuizOrderEntity> findTopByUser_UserIdAndQuiz_CreatedAtBetweenOrderByIdDesc(
+            Long userId,
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+    Optional<QuizOrderEntity> findByUserAndQuiz(
+            UserEntity user,
+            QuizEntity quiz
+    );
+}
