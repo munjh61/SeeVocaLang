ed7961b710256f7ed6024f24ecd852a10ce5fdb0
이인우 <dpfdlalfm@naver.com>
2025-08-07 13:02:53 +0900

    feat: 오늘의 퀴즈 Repository 객체 추가
    - 오늘의 퀴즈가 저장된 테이블에 사용되는 Repository 객체를 추가했습니다.
    
    Resolves: #30

---
+++ backend/src/main/java/com/ssafy/a303/backend/quiz/repository/QuizRepository.java
@@ -0,0 +1,29 @@
+package com.ssafy.a303.backend.quiz.repository;
+
+import com.ssafy.a303.backend.quiz.entity.QuizEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface QuizRepository extends JpaRepository<QuizEntity, Long> {
+
+    int countByCreatedAtGreaterThanEqualAndCreatedAtLessThan(
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+    List<QuizEntity> findAllByProblemNumberGreaterThanEqualAndCreatedAtBetween(
+            int problemNumber,
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+    Optional<QuizEntity> findByProblemNumberAndCreatedAtGreaterThanEqualAndCreatedAtLessThan(
+            int problemNumber,
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+}
