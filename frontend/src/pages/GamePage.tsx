f406650c5a315583da8d074272c9f95856c2cae7
Joonho <junho9661@gmail.com>
2025-08-08 09:53:52 +0900

    game grow 적용

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -2,7 +2,8 @@ import { Navigation } from "../components/organisms/nav/Navigation.tsx";
 
 function GamePage() {
   return (
-    <div className={"flex flex-col h-screen bg-[#F3FAF3]"}>
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow overflow-y-auto"></div>
       <Navigation loc={"game"} />
     </div>
   );
