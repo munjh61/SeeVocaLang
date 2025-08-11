d43de5b8712036ad2c8ecd71c42c1d3cf57342f7
이인우 <dpfdlalfm@naver.com>
2025-08-11 16:45:04 +0900

    fix: FolderService ID 기반 폴더 검색시 삭제여부 옵션 추가
    - 폴더 ID기반으로 엔티티 검색시 삭제되지 않은 것들만 추출하도록 수정했습니다.
    - 단어를 폴더에 저장할 때 사용됩니다.
    
    Resolves: #54

--- backend/src/main/java/com/ssafy/a303/backend/folder/repository/FolderRepository.java
+++ backend/src/main/java/com/ssafy/a303/backend/folder/repository/FolderRepository.java
@@ -2,10 +2,12 @@ package com.ssafy.a303.backend.folder.repository;
 
 import com.ssafy.a303.backend.folder.entity.FolderEntity;
 import com.ssafy.a303.backend.folder.repository.query.FolderQueryRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FolderRepository extends JpaRepository<FolderEntity, Long>, FolderQueryRepository {
+
+    Optional<FolderEntity> findByFolderIdAndIsDeletedFalse(Long folderId);
 
 }
