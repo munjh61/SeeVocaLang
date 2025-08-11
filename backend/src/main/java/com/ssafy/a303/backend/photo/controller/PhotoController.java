122e9f970c5bfe1ff931b1dc8cd6929850cbc993
이인우 <dpfdlalfm@naver.com>
2025-08-11 13:12:22 +0900

    feat: PhotoController 단어 저장가능 폴더목록 조회 API 추가
    Resolves: #45

--- backend/src/main/java/com/ssafy/a303/backend/photo/controller/PhotoController.java
+++ backend/src/main/java/com/ssafy/a303/backend/photo/controller/PhotoController.java
@@ -6,9 +6,13 @@ import com.ssafy.a303.backend.photo.dto.*;
 import com.ssafy.a303.backend.photo.mapper.PhotoMapper;
 import com.ssafy.a303.backend.photo.service.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class PhotoController {
     private static final String UPLOAD_SUCCESS_MESSAGE = "이미지가 성공적으로 업로드되었습니다.";
     private static final String SAVE_WORD_SUCCESS_MESSAGE = "단어가 성공적으로 저장되었습니다.";
     private static final String UPDATE_WORD_SUCCESS_MESSAGE = "단어가 성공적으로 수정되었습니다.";
+    private static final String GET_FOLDERS_CONTAINING_WORDS_SUCCESS_MESSAGE = "단어를 저장가능한 폴더 목록을 성공적으로 조회했습니다.";
 
     private final PhotoService photoService;
 
@@ -55,4 +60,16 @@ public class PhotoController {
                 .message(UPDATE_WORD_SUCCESS_MESSAGE)
                 .build();
     }
+
+    @GetMapping("/api/v1/photos/words/{wordId}/folders")
+    public ResponseEntity<BaseResponseDto<GetFoldersContainingWordsResponseDto>> getFoldersContainingWords(
+            @PathVariable Long wordId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Long userId =  userDetails.getUserId();
+        List<GetFoldersContainingWordsItemDto> folders = photoService.getFoldersContainingWordsList(userId, wordId);
+        GetFoldersContainingWordsResponseDto responseDto = new GetFoldersContainingWordsResponseDto(folders);
+        return ResponseEntity.ok(new BaseResponseDto<>(GET_FOLDERS_CONTAINING_WORDS_SUCCESS_MESSAGE, responseDto));
+    }
+
 }
