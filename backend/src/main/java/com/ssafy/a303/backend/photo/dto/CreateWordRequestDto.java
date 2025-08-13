941e169077c03978e5b0bf7db9caff7fe6c6fb5a
이인우 <dpfdlalfm@naver.com>
2025-08-13 17:29:01 +0900

    feat: 단어 생성 DTO 수정
    - 단어 생성시 Folder 아이디 배열을 받도록 수정했습니다.
    
    Resolves: #65

--- backend/src/main/java/com/ssafy/a303/backend/photo/dto/CreateWordRequestDto.java
+++ backend/src/main/java/com/ssafy/a303/backend/photo/dto/CreateWordRequestDto.java
@@ -2,9 +2,11 @@ package com.ssafy.a303.backend.photo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record CreateWordRequestDto(
         @JsonProperty("name_en")    String nameEn,
         @JsonProperty("name_ko")    String nameKo,
         @JsonProperty("image_key")  String imageKey,
-        @JsonProperty("folder_id")  Long   folderId
+        List<Long> folders
 ) {}
--- backend/src/main/java/com/ssafy/a303/backend/photo/dto/CreateWordResultDto.java
+++ backend/src/main/java/com/ssafy/a303/backend/photo/dto/CreateWordResultDto.java
@@ -1,8 +1,14 @@
 package com.ssafy.a303.backend.photo.dto;
 
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
 public record CreateWordResultDto(
         Long wordId,
         String nameEn,
         String nameKo,
-        String imageUrl
+        String imageUrl,
+        List<CreateWordFolderItemDto> folders
 ) {}
