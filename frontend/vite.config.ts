d2702506f1843778222d7ddc766796d85a06edd0
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-08-17 20:35:43 +0900

    Fix : console 관련 viteconfig 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss(), svgr()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://ec2-13-125-250-93.ap-northeast-2.compute.amazonaws.com", // 로컬 백엔드 주소
+        target: "http://ec2-13-125-250-93.ap-northeast-2.compute.amazonaws.com",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-});
+  esbuild: mode === "production" ? { drop: ["console", "debugger"] } : {},
+}));
