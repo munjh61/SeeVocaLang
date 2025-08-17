ed45771638ba3002e73af254990818bea1913d51
권주현 <joohyun0913@gmail.com>
2025-08-17 14:05:17 +0900

    update friendpage

--- frontend/src/pages/FriendPage.tsx
+++ frontend/src/pages/FriendPage.tsx
@@ -4,25 +4,27 @@ import ocean from "../asset/png/background/background_summer.jpg"
 import parchment from "../asset/png/friend.png"
 
 
+
 function FriendPage() {
+
+  const GAP = "clamp(72px, 12vh, 200px)";
+
   return (
     <div className="relative min-h-screen">
       <div
         className="fixed inset-0 -z-10 bg-cover bg-bottom bg-no-repeat"
         style={{ backgroundImage: `url(${ocean})` }}
       />
-
       <div
-        className="mx-auto w-[min(1100px,calc(100%-32px))] mt-4 rounded-3xl shadow-xl overflow-hidden"
-        style={{
-          backgroundImage: `url(${parchment})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "100% calc(100% - 200px)", 
-          backgroundPosition: "center 90px",
-          minHeight: "80vh",
-        }}
+        className="relative mx-auto w-[min(1100px,calc(100%-32px))] mt-4 rounded-3xl shadow-xl overflow-hidden"
       >
-        <FriendPageTemplate />
+        <div
+          className="absolute inset-x-0 bottom-0 bg-no-repeat bg-[length:100%_100%] bg-center"
+          style={{ top: GAP, backgroundImage: `url(${parchment})` }}
+        />
+        <div className="relative z-10" style={{ paddingTop: GAP, paddingBottom: "96px" }}>
+          <FriendPageTemplate />
+        </div>
       </div>
 
       <div className="fixed inset-x-0 bottom-0 z-50">
